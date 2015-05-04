1bbf5af76cc954a118f6192fd3f9dbb78ac6cc08
nakatani <nakatani.daisuke.839@donuts.ne.jp>
2015-05-04 19:33:59 +0900

    working

--- Keyboard/Catboard.swift
+++ Keyboard/Catboard.swift
@@ -59,7 +59,8 @@ class Catboard: KeyboardViewController {
                         return
                     }
 
-                    textDocumentProxy.insertText("\(randomCat())")
+                    // textDocumentProxy.insertText("\(randomCat())")
+                    textDocumentProxy.insertText("")
                     textDocumentProxy.insertText(" ")
                     textDocumentProxy.insertText(keyOutput)
                     return
@@ -99,40 +100,40 @@ class Catboard: KeyboardViewController {
         return nil
     }
     
-    func takeScreenshotDelay() {
-        var timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("takeScreenshot"), userInfo: nil, repeats: false)
-    }
-    
-    func takeScreenshot() {
-        if !CGRectIsEmpty(self.view.bounds) {
-            UIDevice.currentDevice().beginGeneratingDeviceOrientationNotifications()
-            
-            let oldViewColor = self.view.backgroundColor
-            self.view.backgroundColor = UIColor(hue: (216/360.0), saturation: 0.05, brightness: 0.86, alpha: 1)
-            
-            var rect = self.view.bounds
-            UIGraphicsBeginImageContextWithOptions(rect.size, true, 0)
-            var context = UIGraphicsGetCurrentContext()
-            self.view.drawViewHierarchyInRect(self.view.bounds, afterScreenUpdates: true)
-            var capturedImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            let name = (self.interfaceOrientation.isPortrait ? "Screenshot-Portrait" : "Screenshot-Landscape")
-            var imagePath = "/Users/archagon/Documents/Programming/OSX/RussianPhoneticKeyboard/External/tasty-imitation-keyboard/\(name).png"
-            UIImagePNGRepresentation(capturedImage).writeToFile(imagePath, atomically: true)
-            
-            self.view.backgroundColor = oldViewColor
-        }
-    }
+    // func takeScreenshotDelay() {
+    //     var timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("takeScreenshot"), userInfo: nil, repeats: false)
+    // }
+    // 
+    // func takeScreenshot() {
+    //     if !CGRectIsEmpty(self.view.bounds) {
+    //         UIDevice.currentDevice().beginGeneratingDeviceOrientationNotifications()
+    //         
+    //         let oldViewColor = self.view.backgroundColor
+    //         self.view.backgroundColor = UIColor(hue: (216/360.0), saturation: 0.05, brightness: 0.86, alpha: 1)
+    //         
+    //         var rect = self.view.bounds
+    //         UIGraphicsBeginImageContextWithOptions(rect.size, true, 0)
+    //         var context = UIGraphicsGetCurrentContext()
+    //         self.view.drawViewHierarchyInRect(self.view.bounds, afterScreenUpdates: true)
+    //         var capturedImage = UIGraphicsGetImageFromCurrentImageContext()
+    //         UIGraphicsEndImageContext()
+    //         let name = (self.interfaceOrientation.isPortrait ? "Screenshot-Portrait" : "Screenshot-Landscape")
+    //         var imagePath = "/Users/archagon/Documents/Programming/OSX/RussianPhoneticKeyboard/External/tasty-imitation-keyboard/\(name).png"
+    //         UIImagePNGRepresentation(capturedImage).writeToFile(imagePath, atomically: true)
+    //         
+    //         self.view.backgroundColor = oldViewColor
+    //     }
+    // }
 }
 
-func randomCat() -> String {
-    let cats = "🐱😺😸😹😽😻😿😾😼🙀"
-    
-    let numCats = count(cats)
-    let randomCat = arc4random() % UInt32(numCats)
-    
-    let index = advance(cats.startIndex, Int(randomCat))
-    let character = cats[index]
-    
-    return String(character)
-}
+// func randomCat() -> String {
+//     let cats = "🐱😺😸😹😽😻😿😾😼🙀"
+//     
+//     let numCats = count(cats)
+//     let randomCat = arc4random() % UInt32(numCats)
+//     
+//     let index = advance(cats.startIndex, Int(randomCat))
+//     let character = cats[index]
+//     
+//     return String(character)
+// }
